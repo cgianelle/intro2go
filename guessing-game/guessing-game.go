fe86f2e6abe07cce67d8ffd0d18f486e46599989
Chris Gianelle <gianelc@cisco.com>
2018-08-13 18:14:26 -0400

    added some additional statements depending on outcome

--- guessing-game/guessing-game.go
+++ guessing-game/guessing-game.go
@@ -23,17 +23,28 @@ func main() {
 	fmt.Println("Pick a number between 0 and 100")
 	fmt.Println("You get a maximum of", maxNumGuesses, "guesses")
 
-	for i := 0; i < maxNumGuesses; i++ {
-		fmt.Print("Guess #", i+1, " : ")
+	for i := 1; i <= maxNumGuesses; i++ {
+		fmt.Print("Guess #", i, " : ")
 		n, err = fmt.Scan(&guess)
 		if err == nil && n > 0 {
-			g, _ := strconv.Atoi(guess)
+			g, e := strconv.Atoi(guess)
+			if e != nil {
+				fmt.Println("Huh, I do not recognize what you typed, is it a number? Goodbye")
+				panic(e)
+			}
 			if g > numberToGuess {
 				fmt.Println("You're guess was too high!")
 			} else if g < numberToGuess {
 				fmt.Println("You're guess was too low!")
 			} else {
-				fmt.Println("TIME TO CELEBRATE!!!! AWESOME JOB!!!! You correctly guessed the number.")
+				if i == maxNumGuesses {
+					fmt.Println("Well, somebody got lucky did't they!!! Guessed the correct number with no chances left! ")
+				} else if i == (maxNumGuesses - 1) {
+					fmt.Println("Cutting it close there aren't you! Only one chance remained! But you got it!")
+				} else {
+					fmt.Println("TIME TO CELEBRATE!!!! AWESOME JOB!!!! You correctly guessed the number.")
+				}
+				fmt.Println("Total Number of Guesses:", i)
 				found = true
 				break
 			}
@@ -44,7 +55,8 @@ func main() {
 	}
 
 	if !found {
-		fmt.Println("Too bad that you are terrible at guessing.")
+		fmt.Println("Oh too bad! You're out of guesses.")
+		fmt.Println("Better luck next time!!!")
 		fmt.Println("The correct number was ", numberToGuess)
 	}
 
